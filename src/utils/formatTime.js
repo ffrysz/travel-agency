a7ba8a70d01c1f72ef169bbf213d01c01912f9ee
Filip Frysz <ffrysz@gmail.com>
2021-10-11 23:43:55 +0200

    Add time formating

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,6 +1,13 @@
 export const formatTime = seconds => {
   if (typeof seconds === 'number' && seconds >= 0) {
-    return 'formatted time';
+
+    const formattedSeconds = String(Math.floor(seconds % 60)).padStart(2, '0');
+    const formattedMinutes = String(Math.floor((seconds / 60) % 60)).padStart(2, '0');
+    const formattedHours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const formattedTime = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds;
+
+    return formattedTime;
+
   } else if (!seconds || isNaN(seconds) || seconds < 0) {
     return null;
   }
